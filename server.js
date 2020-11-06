63cbf1d8267773fc383f95e8a019e76b9638dc31
Dailey Kaze <daileykaze@gmail.com>
2020-11-06 10:00:07 -0500

    Created MVP

--- server.js
+++ server.js
@@ -79,62 +79,80 @@ async function addRole(){
     main()
 }
 
+async function addDepartment(){
+    let department = await inquirer.prompt([
+        {
+            message: "What is the Department's title?",
+            name: "name"
+        }
+    ])
+    orm.addDepartment(department)
+    console.log(department)
+    main()
+}
+
+async function updateRole(){
+    let choice = await inquirer.prompt([{
+        type:"list",
+        name: "allRoles",
+        message: "Select which role you would like to update",
+        choices: orm.getAllRoles()
+    },
+    {
+        type:"list",
+        name: "roleTable",
+        message: "What would you like to update?",
+        choices: ["Title", "Salary", "Department"]
+    },
+    {
+        name: "roleInfo",
+        message: "Enter the updated info"
+    }])
+
+    switch(choice.roleInfo){
+        case "Title":
+            orm.updateRole(choice.allRoles, choice.roleTable, choice.roleInfo)
+            break;
+        case "Salary":
+            orm.updateRole(choice.allRoles, choice.roleTable, choice.roleInfo)
+            break;
+        case "Department":
+            orm.updateRole(choice.allRoles, choice.roleTable, choice.roleInfo)
+            break;
+    }
+}
 
 
 async function main(){
-    let users = []
-    let roles = []
-    let departments = []
     let promptAnswers = await inquirer.prompt([{
             type: "list",
             message:"What would you like to do?",
-            choices: ["Add Department", "Add Role", "Add Employee"],
+            choices: ["Add Employee", "Add Role", "Add Department", "View Employees", "View Roles", "View Departments", "Update Role"],
             name: "choiceList"
     }])
-    if (promptAnswers.choiceList === "Add Employee"){
-        let addEmployee = await inquirer.prompt([
-            {
-                message: "What is the employee's first name?",
-                name: "firstName"
-            },
-            {
-                message: "What is the employee's last name?",
-                name: "lastName"
-            },
-            {
-                type: "list",
-                message:"What is the employee's role?",
-                choices: [function(){
-                    if (roles > 0)
-                    orm.getAllRoles()}],
-                name: "role"
-            }
-        ])
-        const employee = new Employee(addEmployee.firstName, addEmployee.lastName, addEmployee.role)
-        orm.addEmployee(employee)
-        users.push(employee)
-    } else if (promptAnswers.choiceList === "Add Role"){
-        let addRole = await inquirer.prompt([
-            {
-                message: "What is the title of this role?",
-                name: "title"
-            },
-            {
-                message: "What is the salary for this role?",
-                name: "salary"
-            }
-        ])
-        const role = new Role(addRole.title, addRole.salary)
-        roles.push(role)
-    } else if (promptAnswers.choiceList === "Add Department"){
-        let addDepartment = await inquirer.prompt([
-            {
-                message: "What is the Department's title?",
-                name: "name"
-            }
-        ])
-        const department = new Department(addDepartment.name)
-        departments.push(department)
+    
+    switch(promptAnswers.choiceList) {
+        case "Add Department":
+            addDepartment()
+            break;
+        case "Add Role":
+            addRole()
+            break;
+        case "Add Employee":
+            addEmployee()
+            break;
+        case "View Employees":
+            viewEmployees()
+            break;
+        case "View Roles":
+            viewRoles()
+            break;
+        case "View Departments":
+            viewDepartments()
+            break;
+        case "Update Role":
+            updateRole()
+            break;
     }
 
 
